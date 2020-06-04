fe334ebb8276ebe2f8f5f62a06a3d79b8d467f4a
oXygen XML Deployer <support@oxygenxml.com>
2020-06-04 13:34:48 +0000

    rebuild pages at 9352751

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,48 @@
+define(function() {var keywords=[{w:"Autumn",p:["p0"]},{w:"Flowers",p:["p0","p9","p10","p11","p12","p13","p14","p26"]},{w:"Bulb",p:["p1"]},{w:"Cultivar",p:["p2"]},{w:"Genus",p:["p3"]},{w:"Panicle",p:["p4"]},{w:"Perennial",p:["p5"]},{w:"Pollination",p:["p6"]},{w:"Rhizome",p:["p7"]},{w:"Sepal",p:["p8"]},{w:"Spring",p:["p9"]},{w:"Summer",p:["p10"]},{w:"Winter",p:["p11"]},{w:"Growing",p:["p12","p13","p14"]},{w:"Index",p:["p13","p30","p35"]},{w:"Terms",p:["p13","p30","p35"]},{w:"-",p:["p13","p14","p30","p31","p35","p36"]},{w:"Search",p:["p14","p31","p36"]},{w:"Results",p:["p14","p31","p36"]},{w:"Garden",p:["p15"]},{w:"Preparation",p:["p15","p17"]},{w:"Pruning",p:["p16"]},{w:"Care",p:["p17"]},{w:"and",p:["p17"]},{w:"Copyright",p:["p18"]},{w:"Chrysanthemum",p:["p19"]},{w:"Gardenia",p:["p20"]},{w:"Gerbera",p:["p21"]},{w:"Iris",p:["p22"]},{w:"Lilac",p:["p23"]},{w:"Salvia",p:["p24"]},{w:"Snowdrop",p:["p25"]},{w:"by",p:["p26"]},{w:"Season",p:["p26"]},{w:"Introduction",p:["p27"]},{w:"Garage",p:["p28","p29","p30","p31","p33"]},{w:"Concepts",p:["p28"]},{w:"Car",p:["p32"]},{w:"Maintenance",p:["p32"]},{w:"Tasks",p:["p33"]},{w:"A",p:["p34","p35","p36"]},{w:"List",p:["p34","p35","p36"]},{w:"of",p:["p34","p35","p36"]},{w:"NASA",p:["p34","p35","p36"]},{w:"Space",p:["p34","p35","p36","p38"]},{w:"Videos",p:["p34","p35","p36"]},{w:"Aeronautics",p:["p37"]},{w:"Deep",p:["p38"]},{w:"Exploration",p:["p38"]},{w:"Movies",p:["p39"]},{w:"Sounds",p:["p40"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2];
+ph["p2"]=[3];
+ph["p3"]=[4];
+ph["p4"]=[5];
+ph["p5"]=[6];
+ph["p6"]=[7];
+ph["p7"]=[8];
+ph["p8"]=[9];
+ph["p9"]=[10, 1];
+ph["p30"]=[14, 15, 16, 35];
+ph["p10"]=[11, 1];
+ph["p32"]=[37, 38];
+ph["p31"]=[17, 18, 16, 35];
+ph["p12"]=[13, 1];
+ph["p34"]=[40, 41, 42, 43, 44, 45];
+ph["p11"]=[12, 1];
+ph["p33"]=[35, 39];
+ph["p14"]=[17, 18, 16, 13, 1];
+ph["p36"]=[17, 18, 16, 40, 41, 42, 43, 44, 45];
+ph["p13"]=[14, 15, 16, 13, 1];
+ph["p35"]=[14, 15, 16, 40, 41, 42, 43, 44, 45];
+ph["p16"]=[21];
+ph["p38"]=[47, 44, 48];
+ph["p15"]=[19, 20];
+ph["p37"]=[46];
+ph["p18"]=[24];
+ph["p17"]=[22, 23, 20];
+ph["p39"]=[49];
+ph["p19"]=[25];
+ph["p40"]=[50];
+ph["p21"]=[27];
+ph["p20"]=[26];
+ph["p23"]=[29];
+ph["p22"]=[28];
+ph["p25"]=[31];
+ph["p24"]=[30];
+ph["p27"]=[34];
+ph["p26"]=[1, 32, 33];
+ph["p29"]=[35];
+ph["p28"]=[35, 36];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
