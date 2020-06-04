78940754e9934c37ed10d75eb5358cb381125e82
oXygen XML Deployer <support@oxygenxml.com>
2020-06-04 09:52:12 +0000

    rebuild pages at 542f50d

--- .travis/publish.sh
+++
@@ -1,9 +0,0 @@
-rm -rf output
-
-# Writes the license key in a file
-# Assumes that $LICENSE_KEY contains the license key with all /n's replaced by @@
-printf "%s" "$LICENSE_KEY" > tools/oxygen-publishing-engine-3.x/licensekey.txt
-sed -i 's/@@/\n/g' tools/oxygen-publishing-engine-3.x/licensekey.txt
-
-tools/oxygen-publishing-engine-3.x/bin/dita -i dita/garage/garage.ditamap -f webhelp-responsive -o output/garage/1.0/ -t temp/garage/1.0 -Dwebhelp.publishing.template.descriptor=material.opt -Dwebhelp.publishing.template=publishing/material/
-tools/oxygen-publishing-engine-3.x/bin/dita -i dita/flowers/flowers.ditamap -f webhelp-responsive -o output/flowers/1.0/ -t temp/flowers/1.0 -Dwebhelp.publishing.template.descriptor=flowers-tiles.opt -Dwebhelp.publishing.template=publishing/flowers/
---
+++ flowers/1.0/oxygen-webhelp/app/nav-links/json/care-d61e27.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Pruning","href":"tasks\/pruning.html","attributes": {"data-id":"prunning",},"menu": {"hasChildren":false,},"tocID":"prunning-d61e38","topics":[]},{"title":"Garden Preparation","href":"tasks\/gardenPreparation.html","attributes": {"data-id":"gardenPreparation",},"menu": {"hasChildren":false,},"tocID":"gardenPreparation-d61e46","topics":[]}]});
