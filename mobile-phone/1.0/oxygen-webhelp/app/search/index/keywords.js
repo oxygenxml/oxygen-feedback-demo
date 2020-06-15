86cd942dc8c3e2ac3ca9219486bbe48ef876110c
oXygen XML Deployer <support@oxygenxml.com>
2020-06-15 12:56:57 +0000

    rebuild pages at 9b3eed8

---
+++ mobile-phone/1.0/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,24 @@
+define(function() {var keywords=[{w:"Composing",p:["p0"]},{w:"and",p:["p0","p3","p13"]},{w:"sending",p:["p0"]},{w:"email",p:["p0","p10"]},{w:"messages",p:["p0","p10"]},{w:"Inserting",p:["p1","p2"]},{w:"the",p:["p1","p2","p6","p9"]},{w:"battery",p:["p1","p6"]},{w:"SIM",p:["p2"]},{w:"card",p:["p2"]},{w:"Locking",p:["p3"]},{w:"unlocking",p:["p3"]},{w:"your",p:["p3"]},{w:"phone",p:["p3","p15","p16"]},{w:"Making",p:["p4"]},{w:"a",p:["p4","p5"]},{w:"call",p:["p4","p5"]},{w:"Setting",p:["p5","p7"]},{w:"up",p:["p5","p7"]},{w:"conference",p:["p5"]},{w:"Removing",p:["p6"]},{w:"an",p:["p7"]},{w:"e-mail",p:["p7","p11"]},{w:"address",p:["p7"]},{w:"Brief",p:["p8"]},{w:"specifications",p:["p8"]},{w:"Adjusting",p:["p9"]},{w:"volume",p:["p9"]},{w:"Deleting",p:["p10"]},{w:"one",p:["p10"]},{w:"or",p:["p10"]},{w:"more",p:["p10"]},{w:"Getting",p:["p12"]},{w:"Started",p:["p12"]},{w:"Important",p:["p13"]},{w:"keys",p:["p13"]},{w:"shortcuts",p:["p13"]},{w:"Introduction",p:["p14"]},{w:"Basics",p:["p15"]},{w:"Setup",p:["p16"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[5, 6, 7];
+ph["p2"]=[5, 6, 8, 9];
+ph["p3"]=[10, 1, 11, 12, 13];
+ph["p4"]=[14, 15, 16];
+ph["p5"]=[17, 18, 15, 19, 16];
+ph["p6"]=[20, 6, 7];
+ph["p7"]=[17, 18, 21, 22, 23];
+ph["p8"]=[24, 25];
+ph["p9"]=[26, 6, 27];
+ph["p10"]=[28, 29, 30, 31, 3, 4];
+ph["p12"]=[32, 33];
+ph["p11"]=[22];
+ph["p14"]=[37];
+ph["p13"]=[34, 35, 1, 36];
+ph["p16"]=[13, 39];
+ph["p15"]=[13, 38];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
--- space-videos/1.0/oxygen-webhelp/app/search/index/keywords.js
+++ space-videos/1.0/oxygen-webhelp/app/search/index/keywords.js
@@ -1,26 +1,10 @@
-define(function() {var keywords=[{w:"Composing",p:["p0"]},{w:"and",p:["p0","p3","p15"]},{w:"sending",p:["p0"]},{w:"email",p:["p0","p12"]},{w:"messages",p:["p0","p12"]},{w:"Inserting",p:["p1","p2"]},{w:"the",p:["p1","p2","p6","p9"]},{w:"battery",p:["p1","p6"]},{w:"SIM",p:["p2"]},{w:"card",p:["p2"]},{w:"Locking",p:["p3"]},{w:"unlocking",p:["p3"]},{w:"your",p:["p3"]},{w:"phone",p:["p3","p18","p19"]},{w:"Making",p:["p4"]},{w:"a",p:["p4","p5"]},{w:"call",p:["p4","p5"]},{w:"Setting",p:["p5","p7"]},{w:"up",p:["p5","p7"]},{w:"conference",p:["p5"]},{w:"Removing",p:["p6"]},{w:"an",p:["p7"]},{w:"e-mail",p:["p7","p13"]},{w:"address",p:["p7"]},{w:"Brief",p:["p8"]},{w:"specifications",p:["p8"]},{w:"Adjusting",p:["p9"]},{w:"volume",p:["p9"]},{w:"Aeronautics",p:["p10"]},{w:"Deep",p:["p11"]},{w:"Space",p:["p11"]},{w:"Exploration",p:["p11"]},{w:"Deleting",p:["p12"]},{w:"one",p:["p12"]},{w:"or",p:["p12"]},{w:"more",p:["p12"]},{w:"Getting",p:["p14"]},{w:"Started",p:["p14"]},{w:"Important",p:["p15"]},{w:"keys",p:["p15"]},{w:"shortcuts",p:["p15"]},{w:"Introduction",p:["p16"]},{w:"Movies",p:["p17"]},{w:"Basics",p:["p18"]},{w:"Setup",p:["p19"]},{w:"Sounds",p:["p20"]}];
+define(function() {var keywords=[{w:"Aeronautics",p:["p0"]},{w:"Deep",p:["p1"]},{w:"Space",p:["p1"]},{w:"Exploration",p:["p1"]},{w:"Introduction",p:["p2"]},{w:"Movies",p:["p3"]},{w:"Sounds",p:["p4"]}];
 var ph={};
-ph["p0"]=[0, 1, 2, 3, 4];
-ph["p1"]=[5, 6, 7];
-ph["p2"]=[5, 6, 8, 9];
-ph["p3"]=[10, 1, 11, 12, 13];
-ph["p4"]=[14, 15, 16];
-ph["p5"]=[17, 18, 15, 19, 16];
-ph["p6"]=[20, 6, 7];
-ph["p7"]=[17, 18, 21, 22, 23];
-ph["p8"]=[24, 25];
-ph["p9"]=[26, 6, 27];
-ph["p10"]=[28];
-ph["p20"]=[45];
-ph["p12"]=[32, 33, 34, 35, 3, 4];
-ph["p11"]=[29, 30, 31];
-ph["p14"]=[36, 37];
-ph["p13"]=[22];
-ph["p16"]=[41];
-ph["p15"]=[38, 39, 1, 40];
-ph["p18"]=[13, 43];
-ph["p17"]=[42];
-ph["p19"]=[13, 44];
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3];
+ph["p2"]=[4];
+ph["p3"]=[5];
+ph["p4"]=[6];
      return {
          keywords: keywords,
          ph: ph
