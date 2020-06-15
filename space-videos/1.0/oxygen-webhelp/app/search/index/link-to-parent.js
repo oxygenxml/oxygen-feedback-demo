cdba85c2bd80f8b84cb3319f6be73d455a230e79
oXygen XML Deployer <support@oxygenxml.com>
2020-06-15 12:51:19 +0000

    rebuild pages at 877fe2c

--- space-videos/1.0/oxygen-webhelp/app/search/index/link-to-parent.js
+++ space-videos/1.0/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {2:-1,3:2,4:2,1:-1,0:-1};
+return {6:14,0:13,5:18,9:18,13:19,16:14,8:14,15:14,4:18,3:18,1:14,12:13,7:13,2:14,18:-1,19:-1,14:-1};
 });
