27ac6d48e8ae23289878340ca53ff7ff95e97b9f
oXygen XML Deployer <support@oxygenxml.com>
2020-09-21 19:58:43 +0000

    rebuild pages at df0942b

--- garage/1.0/oxygen-webhelp/app/search/index/keywords.js
+++ garage/1.0/oxygen-webhelp/app/search/index/keywords.js
@@ -1,8 +1,8 @@
-define(function() {var keywords=[{w:"Garage",p:["p0","p2"]},{w:"Concepts",p:["p0"]},{w:"Car",p:["p1"]},{w:"Maintenance",p:["p1"]},{w:"Tasks",p:["p2"]}];
+define(function() {var keywords=[{w:"Garage",p:["p0","p2"]},{w:"Concepts",p:["p0"]},{w:"Car",p:["p1"]},{w:"Maintenance",p:["p1"]},{w:"-",p:["p1"]},{w:"updated",p:["p1"]},{w:"Tasks",p:["p2"]}];
 var ph={};
 ph["p0"]=[0, 1];
-ph["p1"]=[2, 3];
-ph["p2"]=[0, 4];
+ph["p1"]=[2, 3, 4, 5];
+ph["p2"]=[0, 6];
      return {
          keywords: keywords,
          ph: ph
