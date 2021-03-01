64e20fa5b6395681cc8e2ac8dd1900c6ef95bdbe
oXygen XML Deployer <support@oxygenxml.com>
2021-03-01 12:07:13 +0000

    rebuild pages at 3b6f5b0

--- flowers/1.0/oxygen-webhelp/app/search/index/link-to-parent.js
+++ flowers/1.0/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {0:-1,2:-1,1:-1};
+return {20:10,13:14,18:11,10:23,15:-1,19:9,9:23,0:23,16:0,14:-1,11:23,17:10,24:-1,23:-1,12:14,21:0,22:9};
 });
---
+++ space-videos/1.0/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+define(function() {var keywords=[{w:"Aeronautics",p:["p0"]},{w:"Deep",p:["p1"]},{w:"Space",p:["p1"]},{w:"Exploration",p:["p1"]},{w:"Introduction",p:["p2"]},{w:"Movies",p:["p3"]},{w:"Sounds",p:["p4"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3];
+ph["p2"]=[4];
+ph["p3"]=[5];
+ph["p4"]=[6];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
