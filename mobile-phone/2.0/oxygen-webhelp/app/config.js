59190d356960b016a5fab88d1af19214ac4df0c6
oXygen XML Deployer <support@oxygenxml.com>
2021-03-16 09:37:10 +0000

    rebuild pages at 8960a85

--- mobile-phone/2.0/oxygen-webhelp/app/config.js
+++ mobile-phone/2.0/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210316092536';
+                args = '20210316093657';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- space-videos/1.0/oxygen-webhelp/app/config.js
+++ space-videos/1.0/oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210316092501';
+                args = '20210316093620';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
